8a8e96f9c1537b682adb716d678daee66112d554
Sabrina Boing <sabrinabgbc@email.com>
2018-05-29 23:37:10 -0300

    atualizações de estrutura

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,10 @@
-    	</main>
-    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/foundation.min.js"></script>
-    	<script src="assets/js/main.js"></script>
-    	<footer class="footer">
-            <navbar class="navbar">
-            </navbar>
-        </footer>
-    </body>
-    </html>
+    <footer class="footer">
+        <navbar class="navbar">
+        </navbar>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/foundation.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
--- templates/header.php
+++ templates/header.php
@@ -1,18 +1,17 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-"http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"> 
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
 	<meta name=description content=""/>
 	<meta name=keywords content=""/>
-	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.min.css">
-	<link rel="stylesheet" href="assets/css/main.css"/>
-	<link rel="stylesheet" href="assets/dist/style.css"/>
+	<link rel="stylesheet" href="assets/css/resets.css">
+	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 	<header class="header">
